32156e551a88e34912b2166be15e7cb9c051b220
Daichi HIRATA <bunny.hop.md@gmail.com>
2017-03-28 12:09:40 +0900

    init

---
+++ lib/fluent/plugin/buffer/mysql.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/buffer/mysql/version"
+
+module Fluent
+  module Plugin
+    module Buffer
+      module Mysql
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/buffer/mysql/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Buffer
+      module Mysql
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
